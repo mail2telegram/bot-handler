17770aeacfd4e2f27fa6c4faf636d8a393ac5886
Dmitrii Elkin <7777632@gmail.com>
2020-07-01 18:55:30 +0300

    x-consistent-hash exchange

---
+++ src/QueueLocator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace M2T;
+
+use Redis;
+
+class QueueLocator
+{
+    protected Redis $redis;
+
+    public function __construct(Redis $redis)
+    {
+        $this->redis = $redis;
+    }
+
+    public function lock(): string
+    {
+        $count = App::get('queueAmount');
+        $i = 1;
+        while ($i <= $count) {
+            if ($this->redis->setnx("q$i", true)) {
+                return "q$i";
+            }
+            $i++;
+        }
+        return '';
+    }
+
+    public function release(string $key): void
+    {
+        $this->redis->del($key);
+    }
+}
